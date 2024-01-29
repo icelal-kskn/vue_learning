02ddf74e351f0e771431112b029694e56639aa0b
Celal İkram KESKİN <72507616+icelal-kskn@users.noreply.github.com>
2024-01-29 15:57:36 +0300

    created fake-data.js for every projects

--- e-commerce-with-vue/src/App.vue
+++ e-commerce-with-vue/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/products">Products</router-link> |
+    <router-link to="/products/1">Products Detail Page</router-link> |
+    <router-link to="/cart">Cart</router-link>
   </nav>
   <router-view />
 </template>
---
+++ e-commerce-with-vue/src/fake-data.js
@@ -0,0 +1,141 @@
+/* eslint-disable prettier/prettier */
+import foodPhotos1 from "./assets/foodPhotos/1";
+import foodPhotos2 from "./assets/foodPhotos/2";
+import foodPhotos3 from "./assets/foodPhotos/3";
+import foodPhotos4 from "./assets/foodPhotos/4";
+import foodPhotos5 from "./assets/foodPhotos/5";
+import foodPhotos6 from "./assets/foodPhotos/6";
+import foodPhotos7 from "./assets/foodPhotos/7";
+import foodPhotos8 from "./assets/foodPhotos/8";
+import foodPhotos9 from "./assets/foodPhotos/9";
+import foodPhotos10 from "./assets/foodPhotos/10";
+import foodPhotos11 from "./assets/foodPhotos/11";
+import foodPhotos12 from "./assets/foodPhotos/12";
+import foodPhotos13 from "./assets/foodPhotos/13";
+import foodPhotos14 from "./assets/foodPhotos/14";
+import foodPhotos15 from "./assets/foodPhotos/15";
+import foodPhotos16 from "./assets/foodPhotos/16";
+import foodPhotos17 from "./assets/foodPhotos/17";
+import fakePerson1 from "./assets/fakePerson/1";
+import fakePerson2 from "./assets/fakePerson/2";
+import fakePerson3 from "./assets/fakePerson/3";
+import fakePerson4 from "./assets/fakePerson/4";
+import fakePerson5 from "./assets/fakePerson/5";
+import fakePerson6 from "./assets/fakePerson/6";
+import fakePerson7 from "./assets/fakePerson/7";
+import fakePerson8 from "./assets/fakePerson/8";
+import fakePerson9 from "./assets/fakePerson/9";
+import fakePerson10 from "./assets/fakePerson/10";
+
+export const products = [
+  {
+    id: "123",
+    name: "Running Shoes",
+    price: "60.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
+    imageUrl: foodPhotos1,
+    averageRating: "5.0",
+  },
+  {
+    id: "234",
+    name: "Basketball Shoes",
+    price: "120.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
+    imageUrl: foodPhotos2,
+    averageRating: "5.0",
+  },
+  {
+    id: "345",
+    name: "Bright Red Shoes",
+    price: "90.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
+    imageUrl: foodPhotos3,
+    averageRating: "5.0",
+  },
+  {
+    id: "456",
+    name: "Fancy Shoes",
+    price: "190.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
+    imageUrl: foodPhotos4,
+    averageRating: "5.0",
+  },
+  {
+    id: "567",
+    name: "Skateboard Shoes",
+    price: "75.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
+    imageUrl: foodPhotos5,
+    averageRating: "5.0",
+  },
+  {
+    id: "678",
+    name: "High Heels",
+    price: "200.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
+    imageUrl: foodPhotos6,
+    averageRating: "5.0",
+  },
+  {
+    id: "789",
+    name: "Dark Shoes",
+    price: "100.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
+    imageUrl: foodPhotos7,
+    averageRating: "5.0",
+  },
+  {
+    id: "890",
+    name: "Classic Shoes",
+    price: "40.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
+    imageUrl: foodPhotos8,
+    averageRating: "5.0",
+  },
+  {
+    id: "901",
+    name: "Plain Shoes",
+    price: "54.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
+    imageUrl: foodPhotos9,
+    averageRating: "5.0",
+  },
+  {
+    id: "901",
+    name: "Teal Dress Shoes",
+    price: "330.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
+    imageUrl: foodPhotos10,
+    averageRating: "5.0",
+  },
+  {
+    id: "789",
+    name: "Fancy Boots",
+    price: "230.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
+    imageUrl: foodPhotos11,
+    averageRating: "5.0",
+  },
+  {
+    id: "890",
+    name: "Gold Shoes",
+    price: "180.00",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel enim quam. Mauris nisl tellus, fringilla sed cursus eu, convallis non diam. Mauris quis fringilla nunc. Aenean leo lacus, lobortis sit amet venenatis a, aliquet tristique erat. Etiam laoreet mauris ut dapibus tincidunt. Pellentesque non ex at nisl ornare aliquam sed non ante. Nam lobortis magna id massa cursus, sit amet condimentum metus facilisis. Donec eu tortor at est tempor cursus et sed velit. Morbi rutrum elementum est vitae fringilla. Phasellus dignissim purus turpis, ac varius enim auctor vulputate. In ullamcorper vestibulum mauris. Nulla malesuada pretium mauris, lobortis eleifend dolor iaculis vitae.",
+    imageUrl: foodPhotos12,
+    averageRating: "5.0",
+  },
+];
+
+export const cartItems = [products[0], products[2], products[3]];
