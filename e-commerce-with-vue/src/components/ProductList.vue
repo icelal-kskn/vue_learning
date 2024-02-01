364d59ec24a6eec33bf7ae0c1a2061ed75ba72f7
Celal İkram KESKİN <72507616+icelal-kskn@users.noreply.github.com>
2024-02-01 09:58:59 +0300

    Become more Dynamic

---
+++ e-commerce-with-vue/src/components/ProductList.vue
@@ -0,0 +1,23 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <div v-if="products.length > 0">
+    <ProductsListItem
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    />
+  </div>
+  <p v-else>You haven't added anything to your cart yet!</p>
+</template>
+<!-- eslint-disable prettier/prettier -->
+<script>
+import ProductsListItem from "./ProductsListItem.vue";
+
+export default {
+  name: "ProductsList",
+  props: ["products"],
+  components: {
+    ProductsListItem,
+  },
+};
+</script>
---
+++ e-commerce-with-vue/src/components/ProductsGrid.vue
@@ -0,0 +1,31 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <div class="grid-wrap">
+    <ProductsGridItem
+      v-for="product in products"
+      :key="product.id"
+      :product="product"
+    />
+  </div>
+</template>
+<!-- eslint-disable prettier/prettier -->
+<script>
+import ProductsGridItem from "./ProductsGridItem.vue";
+
+export default {
+  name: "ProductsGrid",
+  props: ["products"],
+  components: {
+    ProductsGridItem,
+  },
+};
+</script>
+<!-- eslint-disable prettier/prettier -->
+<style scoped>
+.grid-wrap {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  margin-top: 16px;
+}
+</style>
