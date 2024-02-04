78b06793fbda0a684616693ab84cc31609152edf
Celal İkram KESKİN <72507616+icelal-kskn@users.noreply.github.com>
2024-02-04 09:09:51 +0300

    Get-Post Operations

--- README.md
+++ README.md
@@ -42,6 +42,10 @@ For Babel
 npm install --save-dev @babel/core @babel/node @babel/preset-env @babel/cli @babel/plugin-transform-runtime
 npm install @babel/runtime
 ```
+Also ypu need this
+```sh
+npm install body-parser
+```
 
 Run the backend with this code
 ```sh
--- e-commerce-backend/src/server.js
+++ e-commerce-backend/src/server.js
@@ -1,13 +1,24 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 
 const app=express();
+app.use(bodyParser.json());
 
 app.get('/hello', (req,res)=> {
     res.send('Hello!');
     }
 );
 
-app.listen(8000,()=>{
-    console.log("Server is listening on port 8000");
+app.get('/hello/:name', (req,res)=>{
+    res.send(`Hello ${req.params.name}!`);
+});
+
+app.post('/hello', (req,res)=> {
+    res.send(`Hello ${req.body.name}!`);
+    }
+);
+
+app.listen(8081,()=>{
+    console.log("Server is listening on port 8081");
 });
